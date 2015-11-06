8d922298f8e281762f4bcf6a0b4d60e990c9ff88
Pierluigi Taddei <pierluigi.taddei@jrc.ec.europa.eu>
2015-11-06 15:13:24 +0100

    qt version for flat download

---
+++ cmake/projects/Qt/cmake/components_version5_5_0.cmake
@@ -0,0 +1,221 @@
+####Component url and fingerprints
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtActiveQt
+	URL "${HUNTER_PACKAGE_URL}/QtActiveQt.tar.gz"
+	SHA1 cc70342ce2002901cc1005251a391c584f4fbc60
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtEnginio
+	URL "${HUNTER_PACKAGE_URL}/QtEnginio.tar.gz"
+	SHA1 3c72b2ae500a238f88a61d0e3d3a06690a5eb5bc
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtANGLE
+	URL "${HUNTER_PACKAGE_URL}/QtANGLE.tar.gz"
+	SHA1 ee9aa1d2cc43651c224f900be16b1681eb605c86
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtBluetooth
+	URL "${HUNTER_PACKAGE_URL}/QtBluetooth.tar.gz"
+	SHA1 4fb1ce9439762a53e4e545b716ee89b64a921cc6
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtCLucene
+	URL "${HUNTER_PACKAGE_URL}/QtCLucene.tar.gz"
+	SHA1 95fe33713ea1eebda32597d08a1e2e0ff3d6ae05
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtCore
+	URL "${HUNTER_PACKAGE_URL}/QtCore.tar.gz"
+	SHA1 aa52ad9c232f1aebdc0d55bcb77967db8e1cfb1d
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtDBus
+	URL "${HUNTER_PACKAGE_URL}/QtDBus.tar.gz"
+	SHA1 b5cbb5bccf088551c11fc2a43b5e1bae2884c556
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtDesigner
+	URL "${HUNTER_PACKAGE_URL}/QtDesigner.tar.gz"
+	SHA1 4cabb98c98a34e20910ed4102bacd5e6f546029a
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtDesignerComponents
+	URL "${HUNTER_PACKAGE_URL}/QtDesignerComponents.tar.gz"
+	SHA1 fece5f88495b72b83f4f4a48a77d312966dd9f09
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtGui
+	URL "${HUNTER_PACKAGE_URL}/QtGui.tar.gz"
+	SHA1 6f0d006f74f2b308f254443b9c5ca531413c3c44
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtHelp
+	URL "${HUNTER_PACKAGE_URL}/QtHelp.tar.gz"
+	SHA1 8aac416219b33c570438864645afb9a0230c77cb
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtMultimedia
+	URL "${HUNTER_PACKAGE_URL}/QtMultimedia.tar.gz"
+	SHA1 80268c1fc42272563d4e3599aa27d0534cf91550
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtMultimediaQuick_p
+	URL "${HUNTER_PACKAGE_URL}/QtMultimediaQuick_p.tar.gz"
+	SHA1 bb15fae88a69178535a8895dc7db8e7d707e138f
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtMultimediaWidgets
+	URL "${HUNTER_PACKAGE_URL}/QtMultimediaWidgets.tar.gz"
+	SHA1 7bd4e6558b7bea8897ed3a241756db873bdf3947
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtNetwork
+	URL "${HUNTER_PACKAGE_URL}/QtNetwork.tar.gz"
+	SHA1 0d649adfcc38b88fcf196aeba8ecc17a9eb825d2
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtNfc
+	URL "${HUNTER_PACKAGE_URL}/QtNfc.tar.gz"
+	SHA1 71785f9b3402decfeef000a8ce664330c37463c0
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtOpenGL
+	URL "${HUNTER_PACKAGE_URL}/QtOpenGL.tar.gz"
+	SHA1 380541ad5c69562913ea9e9831af300b2e31a498
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtOpenGLExtensions
+	URL "${HUNTER_PACKAGE_URL}/QtOpenGLExtensions.tar.gz"
+	SHA1 9bc3483448cbfcc839abe646c0a071f8142b31d0
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtPlatformHeaders
+	URL "${HUNTER_PACKAGE_URL}/QtPlatformHeaders.tar.gz"
+	SHA1 e2c9d7f6ec15fd213eca286a3368a1bd783e5be4
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtPlatformSupport
+	URL "${HUNTER_PACKAGE_URL}/QtPlatformSupport.tar.gz"
+	SHA1 b59de0a10c272093bcf749320a29e6df9a802016
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtPrintSupport
+	URL "${HUNTER_PACKAGE_URL}/QtPrintSupport.tar.gz"
+	SHA1 8c9e27799304663a4e9d1bda0016fd14f764038d
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtQml
+	URL "${HUNTER_PACKAGE_URL}/QtQml.tar.gz"
+	SHA1 34f023e950287bdd9a7938301c03c8ad9240d019
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtQmlDevTools
+	URL "${HUNTER_PACKAGE_URL}/QtQmlDevTools.tar.gz"
+	SHA1 81024fe0d4637b294a303cce4ff591306341cf09
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtQuick
+	URL "${HUNTER_PACKAGE_URL}/QtQuick.tar.gz"
+	SHA1 4f4c3f813ec67c5c4250fa0982dfe576c519571a
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtQuickParticles
+	URL "${HUNTER_PACKAGE_URL}/QtQuickParticles.tar.gz"
+	SHA1 7504a8da44ce0c8a435b43b3f64106b489dfdd19
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtQuickTest
+	URL "${HUNTER_PACKAGE_URL}/QtQuickTest.tar.gz"
+	SHA1 f7dfddf95a6c978d432115926549a2dee91b6029
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtQuickWidgets
+	URL "${HUNTER_PACKAGE_URL}/QtQuickWidgets.tar.gz"
+	SHA1 b050aa31e81d0437078c4d8f7181f980790f8101
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtSensors
+	URL "${HUNTER_PACKAGE_URL}/QtSensors.tar.gz"
+	SHA1 80b0ed3d577ace53460d5f11d0b81de929f04e08
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtSerialPort
+	URL "${HUNTER_PACKAGE_URL}/QtSerialPort.tar.gz"
+	SHA1 222fb0b07da4047b42335b64965f85feb57651f4
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtSql
+	URL "${HUNTER_PACKAGE_URL}/QtSql.tar.gz"
+	SHA1 77ca6aa5426a6ae2e088e6fea0c7891750e34257
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtSvg
+	URL "${HUNTER_PACKAGE_URL}/QtSvg.tar.gz"
+	SHA1 244f6c05fdc622d45963c80d14a5498e105af3c7
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtTest
+	URL "${HUNTER_PACKAGE_URL}/QtTest.tar.gz"
+	SHA1 45bc07315703d7ee083353c8e8592dadb6cda6ea
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtUiPlugin
+	URL "${HUNTER_PACKAGE_URL}/QtUiPlugin.tar.gz"
+	SHA1 bcf76309d13bb98bd3da6cd7203a7f2a92acbad6
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtUiTools
+	URL "${HUNTER_PACKAGE_URL}/QtUiTools.tar.gz"
+	SHA1 c1b3179ef30a2aa399b8fe65104ffabf1e04b6e9
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtWebChannel
+	URL "${HUNTER_PACKAGE_URL}/QtWebChannel.tar.gz"
+	SHA1 a6ebafb2736b40aaff1e79fcfca5c5b92d1dcc3a
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtWebKit
+	URL "${HUNTER_PACKAGE_URL}/QtWebKit.tar.gz"
+	SHA1 381ff091a7f96b00998cc9a358ad3ca173b5a321
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtWebKitWidgets
+	URL "${HUNTER_PACKAGE_URL}/QtWebKitWidgets.tar.gz"
+	SHA1 2615bc1f24270f624b18a187e15d69cd9e9b62a9
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtWebSockets
+	URL "${HUNTER_PACKAGE_URL}/QtWebSockets.tar.gz"
+	SHA1 7b7b840b76c7f202ab6aa89f81ba4654eb71ec43
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtWebView
+	URL "${HUNTER_PACKAGE_URL}/QtWebView.tar.gz"
+	SHA1 47ecb1ad69be418fcefd5b8dd7a04d5b9072f2fd
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtWidgets
+	URL "${HUNTER_PACKAGE_URL}/QtWidgets.tar.gz"
+	SHA1 1dbe6bfa27b5c79d63ed8d3968926f6dfec937d4
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtWinExtras
+	URL "${HUNTER_PACKAGE_URL}/QtWinExtras.tar.gz"
+	SHA1 ba028a04be7722fd9008ef6112a1eb7e078041d9
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtXml
+	URL "${HUNTER_PACKAGE_URL}/QtXml.tar.gz"
+	SHA1 527c3d7a55a9bd50e40133ff77be494b99c254eb
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtXmlPatterns
+	URL "${HUNTER_PACKAGE_URL}/QtXmlPatterns.tar.gz"
+	SHA1 692272d514fd05ecfc7f05577da19fe4831b1023
+)
+hunter_add_component_version( PACKAGE_NAME "Qt" VERSION	"5.5.0"
+	COMPONENT QtZlib
+	URL "${HUNTER_PACKAGE_URL}/QtZlib.tar.gz"
+	SHA1 6eb43ffcd60f354554cfa02be2eac141966f05ae
+)
---
+++ cmake/projects/Qt/cmake/hunter_generate_qt_5_5_info.cmake
@@ -0,0 +1,176 @@
+# Copyright (c) 2015, Ruslan Baratov
+# All rights reserved.
+
+include(hunter_internal_error)
+include(hunter_qt_add_module)
+include(hunter_test_string_not_empty)
+
+# See cmake/projects/Qt/generate.sh
+
+# This function will be used in build scheme too so it's better to set
+# regular CMake variables like WIN32 or ANDROID explicitly by is_{android,win32}
+
+function(
+    hunter_generate_qt_5_5_info
+    component_name
+    skip_components_varname
+    component_depends_on_varname
+    is_android
+    is_win32
+)
+  hunter_test_string_not_empty("${component_name}")
+  hunter_test_string_not_empty("${skip_components_varname}")
+  hunter_test_string_not_empty("${component_depends_on_varname}")
+
+  string(COMPARE NOTEQUAL "${ARGN}" "" has_unparsed)
+  if(has_unparsed)
+    hunter_internal_error("Unparsed argument: ${ARGN}")
+  endif()
+
+  set(
+      all_components
+      qt3d
+      qtactiveqt
+      qtandroidextras
+      qtcore
+      qtcanvas3d
+      qtconnectivity
+      qtdeclarative
+      qtdoc
+      qtenginio
+      qtgraphicaleffects
+      qtimageformats
+      qtlocation
+      qtmacextras
+      qtmultimedia
+      qtquick1
+      qtquickcontrols
+      qtscript
+      qtsensors
+      qtserialport
+      qtsvg
+      qttools
+      qttranslations
+      qtwayland
+      qtwebchannel
+      qtwebengine
+      qtwebkit
+      qtwebkit-examples
+      qtwebsockets
+      qtwinextras
+      qtx11extras
+      qtxmlpatterns
+  )
+
+  # This is modified copy/paste code from <qt-sources>/qt.pro
+
+  if(is_android)
+    set(ANDROID_EXTRAS qtandroidextras)
+  else()
+    set(ANDROID_EXTRAS "")
+  endif()
+
+  if(is_win32)
+    set(ACTIVE_QT qtactiveqt)
+  else()
+    # Project MESSAGE: ActiveQt is a Windows Desktop-only module. Will just generate a docs target.
+    set(ACTIVE_QT "")
+  endif()
+
+  # Order is important. Component of each section should not depends on entry
+  # from section below.
+
+  # Components are in list but not exists in fact:
+  # * qtdocgallery
+  # * qtfeedback
+  # * qtpim
+  # * qtsystems
+
+  # Depends on nothing
+  hunter_qt_add_module(NAME qtcore)
+  # --
+
+  # Depends only on qtcore
+  hunter_qt_add_module(NAME qtandroidextras COMPONENTS qtcore)
+  hunter_qt_add_module(NAME qtmacextras COMPONENTS qtcore)
+  hunter_qt_add_module(NAME qtx11extras COMPONENTS qtcore)
+  hunter_qt_add_module(NAME qtsvg COMPONENTS qtcore)
+  hunter_qt_add_module(NAME qtxmlpatterns COMPONENTS qtcore)
+  hunter_qt_add_module(NAME ${ACTIVE_QT} COMPONENTS qtcore)
+  hunter_qt_add_module(NAME qtimageformats COMPONENTS qtcore)
+  hunter_qt_add_module(NAME qtserialport COMPONENTS qtcore)
+  # --
+
+  # --
+  hunter_qt_add_module(NAME qtdeclarative COMPONENTS qtcore qtsvg qtxmlpatterns)
+  # --
+
+  # Depends only on qtcore/qtdeclarative
+  hunter_qt_add_module(NAME qtcanvas3d COMPONENTS qtdeclarative)
+  hunter_qt_add_module(NAME qtdoc COMPONENTS qtdeclarative)
+  hunter_qt_add_module(NAME qtenginio COMPONENTS qtdeclarative)
+  hunter_qt_add_module(NAME qtgraphicaleffects COMPONENTS qtdeclarative)
+  hunter_qt_add_module(NAME qtmultimedia COMPONENTS qtcore qtdeclarative)
+  hunter_qt_add_module(NAME qtsensors COMPONENTS qtcore qtdeclarative)
+  hunter_qt_add_module(NAME qtwayland COMPONENTS qtcore qtdeclarative)
+  hunter_qt_add_module(NAME qtwebsockets COMPONENTS qtcore qtdeclarative)
+  # --
+
+  # --
+  hunter_qt_add_module(NAME qtquickcontrols COMPONENTS qtdeclarative qtgraphicaleffects)
+  hunter_qt_add_module(NAME qtwinextras COMPONENTS qtcore qtdeclarative qtmultimedia)
+  hunter_qt_add_module(NAME qtlocation COMPONENTS qtcore qtdeclarative)
+  hunter_qt_add_module(NAME qtconnectivity COMPONENTS qtcore ${ANDROID_EXTRAS} qtdeclarative)
+  hunter_qt_add_module(NAME qtwebchannel COMPONENTS qtcore qtdeclarative qtwebsockets)
+  hunter_qt_add_module(NAME qt3d COMPONENTS qtdeclarative qtimageformats)
+  # --
+
+  # --
+  hunter_qt_add_module(NAME qtwebkit COMPONENTS qtdeclarative qtlocation qtmultimedia qtsensors qtwebchannel)
+  # --
+
+  # --
+  hunter_qt_add_module(NAME qttools COMPONENTS qtcore qtdeclarative ${ACTIVE_QT} qtwebkit)
+  hunter_qt_add_module(NAME qtwebengine COMPONENTS qtquickcontrols qtwebchannel qtwebkit qtlocation)
+  # --
+
+  # --
+  hunter_qt_add_module(NAME qtwebkit-examples COMPONENTS qtwebkit qttools)
+  hunter_qt_add_module(NAME qtscript COMPONENTS qtcore qttools)
+  hunter_qt_add_module(NAME qttranslations COMPONENTS qttools)
+  # --
+
+  # --
+  hunter_qt_add_module(NAME qtquick1 COMPONENTS qtscript qtsvg qtxmlpatterns qtwebkit)
+  # --
+
+  string(COMPARE EQUAL "${component_name}" "qtcore" is_qtcore)
+  string(COMPARE EQUAL "${component_${component_name}_depends_on}" "" depends_on_nothing)
+  if(is_qtcore)
+    if(NOT depends_on_nothing)
+      hunter_internal_error("qtcore should not depends on anything")
+    endif()
+  else()
+    if(depends_on_nothing)
+      hunter_internal_error(
+          "component `${component_name}` should have at least one dependency:"
+          " qtcore"
+      )
+    endif()
+  endif()
+
+  set(
+      "${component_depends_on_varname}"
+      "${component_${component_name}_depends_on}"
+      PARENT_SCOPE
+  )
+
+  set(skip_list ${all_components})
+  list(
+      REMOVE_ITEM
+      skip_list
+      "${component_name}"
+      ${component_${component_name}_depends_on}
+  )
+  set("${skip_components_varname}" "${skip_list}" PARENT_SCOPE)
+endfunction()
